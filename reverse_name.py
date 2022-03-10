4fd252450178834673429a921e440e10f942b58f
Mayank Anand <mayankan@gmail.com>
2022-03-10 14:41:34 +0530

    [Mayank] docs: Adding doc string for function in reverse_name program

--- reverse_name.py
+++ reverse_name.py
@@ -1,4 +1,12 @@
 def reverse_name(_f_name, _l_name):
+    """
+    Returns last name and first name separated by space.
+    Parameters:
+        _f_name: First name input by user.
+        _l_name: Last name input by user.
+    Return:
+        string having last name and first name separated by space.
+    """
     try:
         return _l_name + " " + _f_name
     except Exception as e:
