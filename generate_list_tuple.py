f366af2a8bbc53ad53a0feba9d5fa05df895ecd0
Mayank Anand <mayankan@gmail.com>
2022-03-10 14:42:13 +0530

    [Mayank] feat: Adding completed generate_tuple_list program

---
+++ generate_list_tuple.py
@@ -0,0 +1,28 @@
+def gen_list_tuple(_numbers):
+    """
+    Returns List and Tuple for the numbers passed in parameter string separated by comma.
+    Parameter:
+        String containing numbers separated by comma.
+    Return:
+        List and Tuple having numbers given in parameter.
+    """
+    try:
+        num_list = _numbers.split(", ")
+        num_tuple = tuple(num_list)
+        return num_list, num_tuple
+    except Exception as e:
+        print("{} is raised.".format(e))
+
+
+def main():
+    try:
+        numbers = input("Enter numbers separated by comma: ")
+        num_list, num_tuple = gen_list_tuple(numbers)
+        print("List:", num_list)
+        print("Tuple:", num_tuple)
+    except Exception as e:
+        print("{} is raised.".format(e))
+
+
+if __name__ == "__main__":
+    main()
