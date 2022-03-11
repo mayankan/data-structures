f040976521db70343eff5268a7c4aef4599f3b43
Mayank Anand <mayankan@gmail.com>
2022-03-11 11:03:14 +0530

    [Mayank] feat: Adding implemented code for check_no_list python program

---
+++ basic_python/7_check_no.py
@@ -0,0 +1,29 @@
+def check_no_list(list_nos, search_no):
+    """
+    Checks if search_no is present in tuple_nos or not.
+    Parameters:
+        list_nos: Number values in List to be searched in.
+        search_no: Number to be searched in tuple_nos.
+    Return:
+        True if search_no is Present else False
+    """
+    try:
+        for no in list_nos:
+            if no == search_no:
+                return "True"
+        return "False"
+    except Exception as e:
+        print("{} is raised.".format(e))
+
+
+def main():
+    try:
+        list_nos = input("Enter numbers to search in separated by comma: ").split(",")
+        search_no = input("Enter number to search in the above list: ")
+        print(check_no_list(list_nos, search_no))
+    except Exception as e:
+        print("{} is raised.".format(e))
+
+
+if __name__ == "__main__":
+    main()
