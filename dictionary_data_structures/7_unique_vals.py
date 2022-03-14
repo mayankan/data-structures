935694fc81809c7d01fd52defb8afa4286570daa
Mayank Anand <mayankan@gmail.com>
2022-03-14 13:45:52 +0530

    [Mayank] feat: Adding Implemented Unique values in dictionary Program

---
+++ dictionary_data_structures/7_unique_vals.py
@@ -0,0 +1,17 @@
+"""
+    @Author: Mayank Anand
+    @Date: 2022-03-11
+    @Last Modified by: Mayank Anand
+    @Last Modified time: 2022-03-14
+    @Title : Dictionary Data Structure Programs - Unique values in dictionary
+    """
+
+
+def main():
+    sample_data = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": "S005"},
+                   {"V": "S009"}, {"VIII": "S007"}]
+    print("Unique Values:", dict(set(value for d_data in sample_data for value in d_data.values())))
+
+
+if __name__ == "__main__":
+    main()
