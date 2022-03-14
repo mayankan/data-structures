653eecdd1aaae6dba441626a9c44511e8ba2ca74
Mayank Anand <mayankan@gmail.com>
2022-03-14 13:40:19 +0530

    [Mayank] feat: Adding implemented Element addition program

---
+++ dictionary_data_structures/2_add_element.py
@@ -0,0 +1,18 @@
+"""
+    @Author: Mayank Anand
+    @Date: 2022-03-11
+    @Last Modified by: Mayank Anand
+    @Last Modified time: 2022-03-14
+    @Title : Dictionary Data Structure Programs - Adding Key and Value pair in dictionary.
+    """
+
+
+def main():
+    sample_dict = {0: 10, 1: 20}
+    print("Dictionary before adding key:", sample_dict)
+    sample_dict[2] = 30
+    print("Dictionary after adding key:", sample_dict)
+
+
+if __name__ == "__main__":
+    main()
