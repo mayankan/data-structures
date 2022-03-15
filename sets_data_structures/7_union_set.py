c1b6e2bc8b296ebad64f11f2082e42da00195bad
Mayank Anand <mayankan@gmail.com>
2022-03-15 21:06:57 +0530

    [Mayank] refactor: Adding function in Set Union program

--- sets_data_structures/7_union_set.py
+++ sets_data_structures/7_union_set.py
@@ -7,13 +7,26 @@
     """
 
 
+def union_set(set1, set2):
+    """
+    Description:
+        Calculates union of values in Set 1 and values in Set 2.
+    Parameters:
+        set1: Set of certain values.
+        set2: Set of another certain values.
+    Return:
+        All values present in both Set 1 and Set 2.
+    """
+    return set1 | set2
+
+
 def main():
     try:
         set1 = {3, 5, 7, 9, 11, 13, 15, 17, 19}
         set2 = {2, 4, 6, 8, 10, 12, 14, 16, 18}
         print("Set 1: ", set1)
         print("Set 2: ", set2)
-        print("Union of two sets is:", set1 | set2)
+        print("Union of two sets is:", union_set(set1, set2))
     except Exception as e:
         print("{} is raised".format(e))
 
