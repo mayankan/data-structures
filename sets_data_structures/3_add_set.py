8a0e9c3e86f63a303d5774bc6a38517e551d4841
Mayank Anand <mayankan@gmail.com>
2022-03-15 20:45:41 +0530

    [Mayank] refactor: Adding function in add member program

--- sets_data_structures/3_add_set.py
+++ sets_data_structures/3_add_set.py
@@ -7,13 +7,26 @@
     """
 
 
+def add_member(set1):
+    """
+    Description:
+        Takes input from user for member(s) to be added in the given set.
+    Parameter:
+        set1: Set with values in which member(s) are to be added.
+    Return:
+        Set with added members given by the user.
+    """
+    input_vals = int(input("Enter number of values to add in set: "))
+    for val in range(input_vals):
+        input_val = int(input("Input {} value: ".format(val + 1)))
+        set1.add(input_val)
+    return set1
+
+
 def main():
     set1 = {3, 5, 7, 9}
     print("Set: ", set1)
-    input_vals = int(input("Enter number of values to add in set: "))
-    for val in range(input_vals):
-        input_val = int(input("Input {} value: ".format(val+1)))
-        set1.add(input_val)
+    set1 = add_member(set1)
     print("Set after adding member(s):", set1)
 
 
