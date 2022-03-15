41fcb1b95cde8798d8c3498544560578599ee86f
Mayank Anand <mayankan@gmail.com>
2022-03-15 20:10:31 +0530

    [Mayank] fix: Updating Dictionary concatenation using for loop

--- dictionary_data_structures/3_concat_dictionary.py
+++ dictionary_data_structures/3_concat_dictionary.py
@@ -11,11 +11,10 @@ def main():
     dic1 = {1: 10, 2: 20}
     dic2 = {3: 30, 4: 40}
     dic3 = {5: 50, 6: 60}
-    # Adding Dictionary 2 to Dictionary 1.
-    dic1.update(dic2)
-    # Adding Dictionary 3 to merged Dictionary 1 and 2.
-    dic1.update(dic3)
-    print("Concatenated Dictionary is:", dic1)
+    final_dict = {}
+    for inv_dict in (dic1, dic2, dic3):
+        final_dict.update(inv_dict)
+    print("Concatenated Dictionary is:", final_dict)
 
 
 if __name__ == "__main__":
