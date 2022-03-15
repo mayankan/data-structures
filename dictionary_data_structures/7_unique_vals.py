80e2df218ba2e9ba7a343408765ec6bb0b662af3
Mayank Anand <mayankan@gmail.com>
2022-03-15 20:12:37 +0530

    [Mayank] fix: Updating code in Unique values program after syntax fixing

--- dictionary_data_structures/7_unique_vals.py
+++ dictionary_data_structures/7_unique_vals.py
@@ -10,7 +10,7 @@
 def main():
     sample_data = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": "S005"},
                    {"V": "S009"}, {"VIII": "S007"}]
-    print("Unique Values:", dict(set(value for d_data in sample_data for value in d_data.values())))
+    print("Unique Values:", set(value for d_data in sample_data for value in d_data.values()))
 
 
 if __name__ == "__main__":
