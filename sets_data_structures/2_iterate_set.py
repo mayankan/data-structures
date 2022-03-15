86dbe1687d43c9c128c41687d19d929681fd5ff9
Mayank Anand <mayankan@gmail.com>
2022-03-15 09:27:01 +0530

    [Mayank] feat: Adding implemented Iterating Set Program

---
+++ sets_data_structures/2_iterate_set.py
@@ -0,0 +1,17 @@
+"""
+    @Author: Mayank Anand
+    @Date: 2022-03-15
+    @Last Modified by: Mayank Anand
+    @Last Modified time: 2022-03-15
+    @Title : Sets Data Structure Programs - Iterate a set
+    """
+
+
+def main():
+    set1 = {3, 5, 7, 9}
+    for value in set1:
+        print(value)
+
+
+if __name__ == "__main__":
+    main()
