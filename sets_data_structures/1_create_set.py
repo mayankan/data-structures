8e63eb28f55dac96a6f591af0f92c7607ec32da1
Mayank Anand <mayankan@gmail.com>
2022-03-15 09:26:35 +0530

    [Mayank] feat: Adding implemented Set Creation Program

---
+++ sets_data_structures/1_create_set.py
@@ -0,0 +1,16 @@
+"""
+    @Author: Mayank Anand
+    @Date: 2022-03-15
+    @Last Modified by: Mayank Anand
+    @Last Modified time: 2022-03-15
+    @Title : Sets Data Structure Programs - Create a set
+    """
+
+
+def main():
+    set1 = {3, 5, 7, 9}
+    print("\n Set:", set1)
+
+
+if __name__ == "__main__":
+    main()
