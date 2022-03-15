a506ee2198ccb4330f972b43515c676eb10cae7b
Mayank Anand <mayankan@gmail.com>
2022-03-15 21:06:25 +0530

    [Mayank] refactor: Adding function in Remove member if present program

--- sets_data_structures/5_remove_present_set.py
+++ sets_data_structures/5_remove_present_set.py
@@ -7,14 +7,27 @@
     """
 
 
+def discard_member(set1):
+    """
+    Description:
+        Takes input from user for member(s) if present to be removed in the given set.
+    Parameter:
+        set1: Set with values in which member(s) are to be removed.
+    Return:
+        Set with removed members given by the user.
+    """
+    input_vals = int(input("Enter number of values to remove in set: "))
+    for val in range(input_vals):
+        input_val = int(input("Input {} value: ".format(val + 1)))
+        set1.discard(input_val)
+    return set1
+
+
 def main():
     try:
         set1 = {3, 5, 7, 9, 11, 13, 15, 17, 19}
         print("Set: ", set1)
-        input_vals = int(input("Enter number of values to remove in set: "))
-        for val in range(input_vals):
-            input_val = int(input("Input {} value: ".format(val+1)))
-            set1.discard(input_val)
+        set1 = discard_member(set1)
         print("Set after removing member(s):", set1)
     except Exception as e:
         print("{} is raised".format(e))
