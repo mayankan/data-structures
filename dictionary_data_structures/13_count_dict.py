b144292c66b1cae62f4b7d19ff7b1227c3397caf
Mayank Anand <mayankan@gmail.com>
2022-03-15 08:24:25 +0530

    [Mayank] feat: Adding implemented Count of keys in dictionary Python Program

---
+++ dictionary_data_structures/13_count_dict.py
@@ -0,0 +1,19 @@
+"""
+    @Author: Mayank Anand
+    @Date: 2022-03-15
+    @Last Modified by: Mayank Anand
+    @Last Modified time: 2022-03-15
+    @Title : Dictionary Data Structure Programs - Count of keys in dictionary
+    """
+
+
+def main():
+    try:
+        dict_len = {'mayank': 9560, 'anand': 2911, 'prerak': 4923, 'neelesh': 1123, 'farzana': 4355}
+        print("Length : %d" % len(dict_len))
+    except Exception as e:
+        print("{} is raised".format(e))
+
+
+if __name__ == "__main__":
+    main()
