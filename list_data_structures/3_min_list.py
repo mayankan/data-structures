16351b10d3315b90c2a7b8414572621f66e3cc9e
Mayank Anand <mayankanand@192.168.0.200>
2022-03-17 13:14:12 +0530

    [Mayank] fix: Removing logging module in Smallest number from list program

--- list_data_structures/3_min_list.py
+++ list_data_structures/3_min_list.py
@@ -5,7 +5,6 @@
     @Last Modified time: 2022-03-16
     @Title : List Data Structure Programs - Calculates the Smallest number from list
     """
-import logging
 
 
 def min_element(given_list):
@@ -23,18 +22,16 @@ def min_element(given_list):
         # Method 2
         # return min(given_list)
     except Exception as e:
-        logging.exception("{} is raised".format(e))
+        print("{} is raised".format(e))
 
 
 def main():
     try:
-        logging.info("Main function of Smallest element in list program started successfully.")
         list1 = [3, 5, 7, 9, 11, 13, 15, 17, 19]
         print("List:", list1)
         print("Smallest value of all elements in List:", min_element(list1))
-        logging.info("Main function of Smallest element in list program completed successfully.")
     except Exception as e:
-        logging.exception("{} is raised".format(e))
+        print("{} is raised".format(e))
 
 
 if __name__ == "__main__":
