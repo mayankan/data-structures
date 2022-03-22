ff38c17e7a486dbc874607fe0bf5cbab2e309229
Mayank Anand <mayankanand@Mayanks-MacBook-Air.local>
2022-03-22 07:02:23 +0530

    [Mayank] feat: Adding implemented Get Longest Word in List Program

---
+++ strings_data_structures/5_longest_word.py
@@ -0,0 +1,45 @@
+"""
+    @Author: Mayank Anand
+    @Date: 2022-03-19
+    @Last Modified by: Mayank Anand
+    @Last Modified time: 2022-03-19
+    @Title : String Data Structure Programs - Print Longest Word in String List
+    """
+import logger
+
+logger = logger.logging_init('longest_word')
+
+
+def longest_word(word_list):
+    """
+    Description :
+        Searches the longest word in a given list of words and returns the word, and it's length.
+    Parameters :
+        word_list: Given list of words in which the longest word is to be fetched.
+    Return:
+        Word with the longest length amongst the given list of words along with the length of that word.
+    """
+    try:
+        longest_length = len(word_list[0])
+        first_word = word_list[0]
+        for word in word_list:
+            if len(word) > longest_length:
+                longest_length = len(word)
+                first_word = word
+        return first_word, longest_length
+    except Exception as e:
+        print("{} is raised.".format(e))
+
+
+def main():
+    try:
+        sample_list = ["blueberry", "pineapple", "orange", "pie"]
+        logger.info("Word List:", sample_list)
+        word_str, word_len = longest_word(sample_list)
+        logger.info("Longest Word in List: {} and it's length: {}".format(word_str, word_len))
+    except Exception as e:
+        logger.error("{} is raised".format(e))
+
+
+if __name__ == "__main__":
+    main()
