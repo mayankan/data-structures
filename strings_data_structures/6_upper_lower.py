39635ad51478ccbc4e56bf949f93c164abcafcfc
Mayank Anand <mayankan@gmail.com>
2022-03-22 15:51:45 +0530

    [Mayank] docs: Fixing spaces in docstrings in Upper Lowercase Program

--- strings_data_structures/6_upper_lower.py
+++ strings_data_structures/6_upper_lower.py
@@ -3,7 +3,7 @@
     @Date: 2022-03-19
     @Last Modified by: Mayank Anand
     @Last Modified time: 2022-03-19
-    @Title : String Data Structure Programs - String in Upper and Lower cases.
+    @Title: String Data Structure Programs - String in Upper and Lower cases.
     """
 import logger
 
@@ -12,11 +12,11 @@ logger = logger.logging_init('upper_lowercase')
 
 def string_upper_lower(given_string):
     """
-    Description :
+    Description:
         Returns the given string in uppercase and lowercase.
-    Parameters :
+    Parameter:
         given_string: Given string which changed to upper and lower cases.
-    Return :
+    Return:
         Output string in upper and lower cases.
     """
     return given_string.upper(), given_string.lower()
@@ -26,9 +26,9 @@ def main():
     try:
         sample_string = input("Enter a string : ")
         upper_case, lower_case = string_upper_lower(sample_string)
-        logger.info("String in uppercase: {} and lowercase: {}".format(upper_case, lower_case))
+        logger.info(f"String in uppercase: {upper_case} and lowercase: {lower_case}")
     except Exception as e:
-        logger.error("{} is raised".format(e))
+        logger.error(f"{e} is raised")
 
 
 if __name__ == "__main__":
