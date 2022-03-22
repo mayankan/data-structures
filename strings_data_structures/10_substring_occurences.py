0eb1d8d2afe6c9b5837c25573ce84c2cb50d6492
Mayank Anand <mayankan@gmail.com>
2022-03-22 15:52:58 +0530

    [Mayank] docs: Fixing spaces in docstrings in Substring count in String Program

--- strings_data_structures/10_substring_occurences.py
+++ strings_data_structures/10_substring_occurences.py
@@ -3,14 +3,14 @@
     @Date: 2022-03-19
     @Last Modified by: Mayank Anand
     @Last Modified time: 2022-03-19
-    @Title : String Data Structure Programs - String's occurrences in a larger string
+    @Title: String Data Structure Programs - String's occurrences in a larger string
     """
 import logger
 
 logger = logger.logging_init('substring_occurrence')
 
 
-def count_substr_occurrence(given_string, sub_str):
+def count_sub_str_occurrence(given_string, sub_str):
     """
     Description:
         Counts of occurrences of a substring in a string.
@@ -28,10 +28,10 @@ def main():
         sample_string = "Hi Mayank, How are you? Mayank: I am fine, Thankyou."
         logger.info("Given String", sample_string)
         check_occurrence = "Mayank"
-        logger.info("Occurrence of {} in String is {}".format(check_occurrence,
-                            count_substr_occurrence(sample_string, check_occurrence)))
+        logger.info(f"Occurrence of {check_occurrence} in String is "
+                    f"{count_sub_str_occurrence(sample_string, check_occurrence)}")
     except Exception as e:
-        logger.error("{} is raised".format(e))
+        logger.error(f"{e} is raised")
 
 
 if __name__ == "__main__":
