ec068455283501abf93d49affcebd369d0eb7b4b
Mayank Anand <mayankanand@Mayanks-MacBook-Air.local>
2022-03-22 07:04:30 +0530

    [Mayank] feat: Adding implemented Reverse String Program

---
+++ strings_data_structures/11_reverse_string.py
@@ -0,0 +1,34 @@
+"""
+    @Author: Mayank Anand
+    @Date: 2022-03-19
+    @Last Modified by: Mayank Anand
+    @Last Modified time: 2022-03-19
+    @Title : String Data Structure Programs - Reverse string
+    """
+import logger
+
+logger = logger.logging_init('reverse_string')
+
+
+def string_reverse(given_string):
+    """
+    Description:
+        Reverses the given string.
+    Parameters:
+        given_string: Given string that is to be reversed.
+    Return:
+        Reversed string.
+    """
+    return given_string[::-1]
+
+
+def main():
+    try:
+        input_string = input("Enter a string : ")
+        logger.info("Reversed String :", string_reverse(input_string))
+    except Exception as e:
+        logger.error("{} is raised".format(e))
+
+
+if __name__ == "__main__":
+    main()
