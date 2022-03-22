49fd3d2c0a7a5135e77a923ece9b2d74fb236802
Mayank Anand <mayankan@gmail.com>
2022-03-22 15:52:27 +0530

    [Mayank] docs: Fixing spaces in docstrings in Suffix of Substring Program

--- strings_data_structures/8_suffix_char.py
+++ strings_data_structures/8_suffix_char.py
@@ -3,7 +3,7 @@
     @Date: 2022-03-19
     @Last Modified by: Mayank Anand
     @Last Modified time: 2022-03-19
-    @Title : String Data Structure Programs - Suffix of String after Splitting
+    @Title: String Data Structure Programs - Suffix of String after Splitting
     """
 import logger
 
@@ -14,21 +14,21 @@ def suffix_char(given_string):
     """
     Description:
         Splits the given string based on '-' seperator.
-    Parameters:
+    Parameter:
         given_string: Given string that is to be split.
     Return:
         Suffix of string after '-' character.
     """
-    return given_string.rsplit('-',1)[0]
+    return given_string.rsplit('-', 1)[-1]
 
 
 def main():
     try:
         sample_string = 'https://www.w3resource.com/python-exercises'
-        logger.info("Given String:", sample_string)
-        logger.info("The Last Part of String after separation is:", suffix_char(sample_string))
+        logger.info(f"Given String: {sample_string}")
+        logger.info(f"The Last Part of String after separation is: {suffix_char(sample_string)}")
     except Exception as e:
-        logger.error("{} is raised".format(e))
+        logger.error(f"{e} is raised")
 
 
 if __name__ == "__main__":
