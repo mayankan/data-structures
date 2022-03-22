40f5e1679b24387b83f40b124c29d673cda9f209
Mayank Anand <mayankan@gmail.com>
2022-03-22 15:50:15 +0530

    [Mayank] docs: Fixing spaces in docstrings in Character Frequency Program

--- strings_data_structures/2_character_frequency.py
+++ strings_data_structures/2_character_frequency.py
@@ -3,7 +3,7 @@
     @Date: 2022-03-17
     @Last Modified by: Mayank Anand
     @Last Modified time: 2022-03-17
-    @Title : String Data Structure Programs - Character Frequency of String
+    @Title: String Data Structure Programs - Character Frequency of String
     """
 import logger
 
@@ -14,7 +14,7 @@ def char_frequency(given_string):
     """
     Description:
         Counts the number of occurrences for each character in the given string.
-    Parameters:
+    Parameter:
         given_string: Given string whose character frequency is to be calculated.
     Return:
         Dictionary which contains unique characters from given string as key and number of occurrences
@@ -22,9 +22,9 @@ def char_frequency(given_string):
     """
     result_dict = {}
     for character in given_string :
-        if character in result_dict.keys() :
+        if character in result_dict.keys():
             result_dict[character] += 1
-        else :
+        else:
             result_dict[character] = 1
     return result_dict
 
@@ -32,10 +32,10 @@ def char_frequency(given_string):
 def main():
     try:
         sample_string = "Mayank"
-        logger.info("Given String:", sample_string)
-        logger.info("Character Frequency of Given String is:", char_frequency(sample_string))
+        logger.info(f"Given String: {sample_string}")
+        logger.info(f"Character Frequency of Given String is: {char_frequency(sample_string)}")
     except Exception as e:
-        logger.error("{} is raised".format(e))
+        logger.error(f"{e} is raised")
 
 
 if __name__ == "__main__":
